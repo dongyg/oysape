a694d73f13f11ccd60a826add7784b28ce2ee417
Dong Yiguang <dongyg@qq.com>
2024-05-08 12:50:35 -0700

    Can enable debug for www.py

--- src/www.py
+++ src/www.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, signal
-from helpers import server, console
+import sys, signal, argparse
+from helpers import server, console, consts
 
 def sig_handler(signum, frame):
     raise SystemExit()
 
 if __name__ == '__main__':
+    version = '2024.0507.1'
+    parser = argparse.ArgumentParser(description="Oysape Webhost Entry Point")
+    parser.add_argument('--debug', type=bool, help="Debug mode", default=False)
+    parser.add_argument('--port', type=int, help="Port", default=19790)
+    args = parser.parse_args()
+
+    consts.initVariants(args.debug, version)
+
     signal.signal(signal.SIGTERM, sig_handler)
     if sys.argv and sys.argv[0] == 'src/www.py':
         # run in interactive mode
-        server.start_http_server('0.0.0.0', len(sys.argv) > 1 and int(sys.argv[1]) or 19790)
+        server.start_http_server('0.0.0.0', int(args.port or 19790))
         console.embed()
     else:
         # run in server mode
-        server.open_http_server('0.0.0.0', len(sys.argv) > 1 and int(sys.argv[1]) or 19790)
+        server.open_http_server('0.0.0.0', int(args.port or 19790))
     print('Bye.')

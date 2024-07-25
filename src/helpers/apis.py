7c82d87b1e58aff1278a5b0f0ef90964b433c744
Dong Yiguang <dongyg@qq.com>
2024-07-25 11:19:58 -0700

    fix install webhost

--- src/helpers/apis.py
+++ src/helpers/apis.py
@@ -1255,7 +1255,7 @@ class ApiDesktop(ApiOverHttp):
             self.combinedConnections[serverKey].onChannelString((CRLF+'Running webhost container...'))
             oneTimeSecret = obhs.keys.get(obh) or tools.getRandomString(60)
             # cmd1 = self.combinedConnections[serverKey].dockerCommandPrefix + f'docker run --rm --name {containerName} -p {portMapping} -e WEBHOST_CONFIG=' + (oneTimeSecret+'@'+obh) + ' -e GITHUB_WEBHOOK_SECRET='+(params.get('github_hook_secret') or '') + '-e BITBUCKET_WEBHOOK_SECRET=' + (params.get('bitbucket_hook_secret') or '') + f' {volumes} -itd oysape/webhost'
-            cmd1 = self.combinedConnections[serverKey].dockerCommandPrefix + f'docker run --name {containerName} -p {portMapping} -e WEBHOST_CONFIG=' + (oneTimeSecret+'@'+obh) + ' -e GITHUB_WEBHOOK_SECRET='+(params.get('github_hook_secret') or '') + '-e BITBUCKET_WEBHOOK_SECRET=' + (params.get('bitbucket_hook_secret') or '') + f' {volumes} -itd oysape/webhost'
+            cmd1 = self.combinedConnections[serverKey].dockerCommandPrefix + f'docker run --name {containerName} -p {portMapping} -e WEBHOST_CONFIG=' + (oneTimeSecret+'@'+obh) + ' -e GITHUB_WEBHOOK_SECRET='+(params.get('github_hook_secret') or '""') + '-e BITBUCKET_WEBHOOK_SECRET=' + (params.get('bitbucket_hook_secret') or '""') + f' {volumes} -itd oysape/webhost'
             # self.dockerExecCommand({'command': cmd1, 'target': serverKey, 'output': True})
             retcmd = self.combinedConnections[serverKey].execute_command(cmd1)
             self.combinedConnections[serverKey].onChannelString((CRLF+retcmd))

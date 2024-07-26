540cc74c7603137fde5c0e8899072c9701ba6f21
Dong Yiguang <dongyg@qq.com>
2024-07-25 18:00:34 -0700

    fix webhost

--- src/components/Websites/WebsiteManage.jsx
+++ src/components/Websites/WebsiteManage.jsx
@@ -74,7 +74,7 @@ from helpers import apis
 
 def handle_github_event(headers, payload):
     # Call the user-defined function
-    event_type = headers.get('X-GitHub-Event')
+    event_type = headers.get('X-Github-Event')
     print(f"GitHub event: {event_type}")
     print(headers)
     print(payload)
@@ -84,7 +84,7 @@ def handle_github_event(headers, payload):
         # Call a task or pipeline if API object is found. runMode=command will get the output synchronously
         print(apiObj.callTask({'runMode':'command', 'taskKey':'hello', 'serverKey':'localhost'}))
         # Send a notification to users by emails
-        print(apiObj.sendNotification({'recipients':[], 'title':"Hello, World!", 'message':"Hello, World!"}))
+        print(apiObj.sendNotification({'recipients':['abc@example.com'], 'title':"Hello, World!", 'message':"Hello, World!"}))
 
 
 def handle_bitbucket_event(headers, payload):
@@ -99,7 +99,7 @@ def handle_bitbucket_event(headers, payload):
         # Call a task or pipeline if API object is found. runMode=command will get the output synchronously
         print(apiObj.callPipeline({'runMode':'command', 'pipelineName':'hello world'}))
         # Send a notification to users by emails
-        print(apiObj.sendNotification({'recipients':[], 'title':"Hello, World!", 'message':"Hello, World!"}))
+        print(apiObj.sendNotification({'recipients':['abc@example.com'], 'title':"Hello, World!", 'message':"Hello, World!"}))
 `
 
 const WebsiteManage = ({ uniqueKey, websiteKey, websiteObject}) => {

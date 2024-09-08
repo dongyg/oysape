4592fe81f699948c5acd2d8831ae2f4db10f1a5e
Dong Yiguang <dongyg@qq.com>
2024-09-08 08:14:40 -0700

    Prompt while starting websocket failed

--- src/index.py
+++ src/index.py
@@ -5,6 +5,19 @@ import argparse, logging
 import webview
 from helpers import server, consts, apis
 
+# HTML 内容作为字符串
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Oysape</title>
+</head>
+<body>
+    <div width="100%" height="100%" style="display: flex; align-items: center; justify-content: center;"><h1>Failed to start websocket server.</h1></div>
+</body>
+</html>
+"""
+
 def initialize_app():
     version = '3.8.12'
     # os_info = distro.name(pretty=True) if platform.system() == 'Linux' else platform.platform()
@@ -19,15 +32,18 @@ def initialize_app():
 
     consts.initVariants(args.debug, version)
 
+    apis.apiInstances[webview.token] = apis.ApiDesktop(clientId=webview.token, clientUserAgent=clientAgent)
     if server.start_http_server():
-        apis.apiInstances[webview.token] = apis.ApiDesktop(clientId=webview.token, clientUserAgent=clientAgent)
         windowObj = apis.loadEntrypointWindow(apiObject=apis.apiInstances[webview.token])
         # Give private_mode=False to save cookies persistently
         webview.start(apis.mainloop, windowObj, debug=consts.IS_DEBUG, private_mode=False, user_agent=clientAgent)
-        print('Bye.')
     else:
         print("Failed to start websocket server.")
         logging.error("Failed to start websocket server.")
+        webview.create_window('Oysape'+(' - Debug' if consts.IS_DEBUG else ''), html=html_content, width=800, height=600, confirm_close=False)
+        webview.start()
+
+    print('Bye.')
 
 
 if __name__ == '__main__':
